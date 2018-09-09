300446275fb545f29c79b0334c2e0c7214629997
Amy <amy.tai0120@gmail.com>
2018-09-09 18:18:53 +0000

    initial commit of Q3 without css

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(function () {
+  let amenityDict = {};
+  $('input[type=checkbox]').change(function () {
+    if(this.checked) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    let amenityValues = Object.values(amenityDict);
+    if (amenityValues.length > 0) {
+      $('div.amenities > h4').text(amenityValues.join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+
+  .get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus)
+    {
+      alert("Done, with the following status: " + textStatus + ". Here is the response: " + data);
+      if(textStatus == 'success') {
+        $(div#api_status).addClass('available');
+      } else {
+        $(div#api_status).removeClass('available');
+      }
+    });
+});
+
