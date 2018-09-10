7c3364cca6ec713db324231fd9580db50b29a0c6
Amy <amy.tai0120@gmail.com>
2018-09-10 04:45:20 +0000

    added ajax functionality for rendering places on front-end

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -14,12 +14,50 @@ $(function () {
     }
   });
 
-  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus)
-    {
-      if(textStatus === 'success') {
-        $('div#api_status').addClass('available');
-        } else {
-          $('div#api_status').removeClass('available');
-        }
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
+    if(textStatus === 'success') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+      }
+  });
+
+  $.ajax(
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'post',
+      data: {},
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        $.each(data, function(i, place) {
+          $('SECTION.places').append(
+            '<article>'
+              + '<div class="title">'
+              +   '<h2>' + place.name + '</h2>'
+              +     '<div class="price_by_night">'
+              +       place.price_by_night
+              +     '</div>'
+              + '</div>'
+              + '<div class="information">'
+              +   '<div class="max_guest">'                                                           
+              +     '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />'
+              +       place.max_guest + "Guests"
+              +     '</div>'
+              +     '<div class="number_rooms">'
+              +       '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />'
+              +         place.number_rooms + "Bedrooms"
+              +     '</div>'
+              +   '<div class="number_bathrooms">'
+              +     '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />'
+              +       place.number_bathrooms + "Bathrooms"
+              +     '</div>'
+              +   '</div>'
+              +   '<div class="description">'
+              +     place.description
+              +   '</div>'
+              + '</article>'
+          );
+        });
     });
+  });
 });
