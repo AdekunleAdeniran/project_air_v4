7e28fa75f0528395b42e957975eb80600ce45125
Amy <amy.tai0120@gmail.com>
2018-09-10 02:11:50 +0000

    fixed syntax the js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,13 +14,12 @@ $(function () {
     }
   });
 
-  .get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus)
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus)
     {
-      if(textStatus == 'success') {
-        $(div#api_status).addClass('available');
-      } else {
-        $(div#api_status).removeClass('available');
-      }
+      if(textStatus === 'success') {
+        $('div#api_status').addClass('available');
+        } else {
+          $('div#api_status').removeClass('available');
+        }
     });
 });
-
